024640fc33b36c0b68e3f744ac1b07fa63a757ce
Harrison Song <harrison.song@gmail.com>
2017-07-27 18:19:21 -0400

    Second commit with java code
    HelloDate.java and HelloWorld.java files.

---
+++ HelloDate.java
@@ -0,0 +1,13 @@
+//: operators/HelloDate.java
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+public class HelloDate {
+	
+	public static void main(String[] args) {
+		print("Today the day is:");
+		print(new Date());
+		System.out.print("Modified the code");
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.io.*;
+
+/**
+ * This Class is a testing class for Harrison to learn Java
+ * @author HarrisonS
+ *
+ */
+public class HelloWorld {
+	
+/**
+ * This is the main entrance for the Java Program
+ * @param argv
+ */
+	public static void main(String[] argv) {
+		System.out.println("Hello World!!!");
+		System.out.println("Under Git Control now!");
+	}
+
+}
